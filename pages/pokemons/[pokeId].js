bf6770e362bd5008744e6581dafd7e673338deb8
renata-souza <renatansmto@gmail.com>
2022-09-07 23:56:45 -0300

    feat: add new dynamic pages using pokeId as path

--- pages/pokemons/[pokeId].js
+++ pages/pokemons/[pokeId].js
@@ -1,11 +1,38 @@
-import React from 'react'
+export const getStaticPaths = async () => {
+  const maxPokemons = 151
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${maxPokemons}`)
+  const pokemons = await response.json()
 
-const pokemon = () => {
-  
+  const paths = pokemons.results.map((pokemon, index) => {
+    return {
+      params: { pokeId: (index + 1).toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pokeId
+
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const pokemon = await response.json()
+
+  return {
+    props: { pokemon: pokemon }
+  }
+}
+
+const pokemons = ({ pokemon }) => {
 
   return (
-    <div>pokemon</div>
+    <>
+      <div>Pokemon: {pokemon.name}</div>
+    </>
   )
 }
 
-export default pokemon
+export default pokemons
