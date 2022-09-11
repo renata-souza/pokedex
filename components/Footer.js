29db3f61b7f1fecd12458d8ed724d0012b7b7086
renata-souza <renatansmto@gmail.com>
2022-09-10 21:16:55 -0300

    refactor: change footer styles to globals.css

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import styles from '../styles/Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className='footer'>
       Developed by Renata Souza
     </footer>
   )
